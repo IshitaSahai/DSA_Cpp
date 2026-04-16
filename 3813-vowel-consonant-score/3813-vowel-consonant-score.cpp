f80dc20a8dd3fc1faaa4e4a5c8c7890d65651bb2
Ishita Sahai <ishitasahai0999@gmail.com>
2026-04-16 19:45:36 +0530

    Time: 0 ms (100%), Space: 9.2 MB (14.78%) - LeetHub

---
+++ 3813-vowel-consonant-score/3813-vowel-consonant-score.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int vowelConsonantScore(string s) {
+        int n=s.length();
+        int c=0, v=0;
+        for(int i=0;i<n;i++){
+            if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u'){
+                v+=1;
+            }else if(s[i]>='a' && s[i]<'z' || s[i]>'a' && s[i]<='z'){
+                c+=1;
+            }
+        }
+        int score=0;
+        if(c>0){
+            score=floor(v/c);
+        }else{
+            score=0;
+        }
+        return score;
+    }
+};
