d13bd6853cbf7b8e9bb278887aad9ad2838bf49c
Ishita Sahai <ishitasahai0999@gmail.com>
2026-05-26 09:48:56 +0530

    Time: 0 ms (100%), Space: 8.4 MB (17.06%) - LeetHub

---
+++ 0504-base-7/0504-base-7.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string convertToBase7(int num) {
+        int rem=0, sn=0, cnt=0;
+        int n=abs(num);
+        string s="", temps="";
+        while(n>0){
+            rem=n%7;
+            n/=7;
+            // cnt++;
+            // sn=(sn*10)+rem;
+            // if(cnt==1 && rem==0){
+            //     temps="0";
+            // }
+            temps+=to_string(rem);
+        }
+        if(num==0){
+            return "0";
+        }else if(num>0){
+            reverse(temps.begin(),temps.end());
+            s=temps;
+        }else{
+            temps+="-";
+            reverse(temps.begin(),temps.end());
+            s=temps;
+        }
+        return s;
+    }
+};
