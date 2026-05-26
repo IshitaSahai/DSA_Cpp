e560db488b2c13df052bfeb97f177ad34b570e37
Ishita Sahai <ishitasahai0999@gmail.com>
2026-05-26 10:10:38 +0530

    Time: 0 ms (100%), Space: 8 MB (68.15%) - LeetHub

--- 0521-longest-uncommon-subsequence-i/0521-longest-uncommon-subsequence-i.cpp
+++ 0521-longest-uncommon-subsequence-i/0521-longest-uncommon-subsequence-i.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     int findLUSlength(string a, string b) {
-        int cnt=0, i=0, j=0;
         int n=a.length(), m=b.length();
         if(a==b) return -1;
         else{
