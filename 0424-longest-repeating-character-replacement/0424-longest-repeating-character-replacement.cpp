2f00de249f17e47191b30ae7d5ad1b510b084b5d
Ishita Sahai <ishitasahai0999@gmail.com>
2026-06-02 06:38:17 +0530

    Time: 7 ms (45.38%), Space: 10.9 MB (39.79%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -8,9 +8,9 @@ public:
             mpp[s[r]]++;
             maxfreq=max(maxfreq,mpp[s[r]]);
             changes=(r-l+1)-maxfreq;
-            if(changes>k){
+            if(changes>k){//no need for while loop as no need to trim it dowm to too small length as the next answer would be the one that would be >=maxi ie. the maxlen so no need to trim it down till <maxi
                 mpp[s[l]]--;
-                maxfreq=0;
+                maxfreq=0;//no need to update maxfreq to a smaller value than the already present max value
                 l++;
             }
             if(changes<=k){
